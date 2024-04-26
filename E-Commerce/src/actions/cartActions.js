8b9ad73f0c24eb8c4c9ad1f2ca84d5361d5ec6aa
Umaradigun <adigunfaruk09061@gmail.com>
2024-04-26 03:16:18 -0400

    first commit

---
+++ E-Commerce/src/actions/cartActions.js
@@ -0,0 +1,28 @@
+import { ADD_TO_CART, REMOVE_FROM_CART, DECREASE_QUANTITY, NEW_QUANTITY } from "./types"
+
+export const addToCart = item => dispatch => {
+  dispatch({
+    type: ADD_TO_CART,
+    item,
+  })
+}
+
+export const removeFromCart = item => dispatch => {
+  dispatch({
+    type: REMOVE_FROM_CART,
+    item,
+  })
+}
+
+export const decreaseQuantity = item => dispatch => {
+  dispatch({
+    type: DECREASE_QUANTITY,
+    item,
+  })
+}
+export const  new_quantity = item => dispatch =>{
+  dispatch(
+    type: NEW_QUANTITY,
+    item,
+  )
+}
